5e52c64cc52e0f4533f3d9b95fa2cc1853b96f1d
Liviu Ignat <liviu@ignat.email>
2015-07-20 13:29:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Fetch feeds from twitter.
---
+++ karma.conf.js
@@ -0,0 +1,52 @@
+module.exports = function(config) {
+  'use strict';
+
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: [
+      'test/phantom-polyfill.js',
+      'test/jasmine-aliases.js',
+      'node_modules/karma-babel-preprocessor/node_modules/babel-core/browser-polyfill.js',
+      'src/**/*.js',
+      'test/**/*.js'
+    ],
+    exclude: [],
+    reporters: ['spec'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['PhantomJS'],
+    captureTimeout: 60000,
+    singleRun: false,
+
+    preprocessors: {
+      'src/**/*.js': ['babel'],
+      'test/**/*.js': ['babel'],
+      'src/**/*.coffee': ['coffee'],
+      'test/**/*.coffee': ['coffee']
+    },
+
+    'babelPreprocessor': {
+      options: {
+        sourceMap: 'inline'
+      },
+      filename: function(file) {
+        return file.originalPath.replace(/\.js$/, '.es5.js');
+      },
+      sourceFileName: function(file) {
+        return file.originalPath;
+      }
+    },
+
+    'coffeePreprocessor': {
+      options: {
+        sourceMap: true
+      },
+      transformPath: function(path) {
+        return path.replace(/\.coffee$/, '.js');
+      }
+    }
+  });
+};
